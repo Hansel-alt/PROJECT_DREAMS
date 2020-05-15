e587450c5379186964ad4cb6b909a4d8fbb6f7cd
Hansel-alt <60404509+Hansel-alt@users.noreply.github.com>
2020-05-15 12:32:56 -0400

    Add files via upload

--- APP/public/js/search.js
+++ APP/public/js/search.js
@@ -1,65 +1,61 @@
 const base_url = "https://api.jikan.moe/v3";
 
-
 function searchAnime(event) {
-
     event.preventDefault();
 
     const form = new FormData(this);
     const query = form.get("search");
 
     fetch(`${base_url}/search/anime?q=${query}&page=1`)
-        .then(res => res.json())
+        .then((res) => res.json())
         .then(updateDom)
-        .catch(err => console.warn(err.message));
+        .catch((err) => console.warn(err.message));
 }
 
 function updateDom(data) {
+    const searchResults = document.getElementById("search-results");
 
-    const searchResults = document.getElementById('search-results');
+    const animeByCategories = data.results.reduce((acc, anime) => {
+        const { type } = anime;
+        if (acc[type] === undefined) acc[type] = [];
+        acc[type].push(anime);
+        return acc;
+    }, {});
 
-    const animeByCategories = data.results
-        .reduce((acc, anime) => {
-
-            const { type } = anime;
-            if (acc[type] === undefined) acc[type] = [];
-            acc[type].push(anime);
-            return acc;
-
-        }, {});
-
-    searchResults.innerHTML = Object.keys(animeByCategories).map(key => {
-
-        const animesHTML = animeByCategories[key]
-            .sort((a, b) => a.episodes - b.episodes)
-            .map(anime => {
-                return `<div class="w3-card-4" style="width:50%">
+    searchResults.innerHTML = Object.keys(animeByCategories)
+        .map((key) => {
+            const animesHTML = animeByCategories[key]
+                .sort((a, b) => a.episodes - b.episodes)
+                .map((anime) => {
+                    return `<div class="w3-card-4" style="width:50%">
+                        <div class"w3-conatainer">
                         <img src="${anime.image_url}" style="width:100%">
+                        </div>
                         <div class="w3-container w3-center">
                             <span class="card-title">${anime.title}</span>
                             <p>${anime.synopsis}</p>
                         </div>
-                        <div class="w3-container-action w3-center">
-                            <a style="text-decoration: none;" href="${anime.url}">Find out more</a>
+                        <div class="w3-container w3-center">
+                            <a style="text-decoration: none;" href="${anime.url}">Find Out More</a>
                         </div>
                     </div>
-                `
-            }).join("");
+                `;
+                })
+                .join("");
 
-
-        return `
+            return `
                 <section>
                     <h3>${key.toUpperCase()}</h3>
                     <div class="op">${animesHTML}</div>
                 </section>
-            `
-    }).join("");
+            `;
+        })
+        .join("");
 }
 
 function pageLoaded() {
-    const form = document.getElementById('search_form');
+    const form = document.getElementById("search_form");
     form.addEventListener("submit", searchAnime);
 }
-
 
 window.addEventListener("load", pageLoaded);
